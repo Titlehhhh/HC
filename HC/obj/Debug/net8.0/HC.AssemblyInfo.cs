59a94f60ebec90af8e7c2640d41e68d2cd6c61d5
TitleHHHH <TitleHHHH@outlook.com>
2024-11-03 13:28:15 +0500

    a

--- HC/obj/Debug/net8.0/HC.AssemblyInfo.cs
+++ HC/obj/Debug/net8.0/HC.AssemblyInfo.cs
@@ -12,11 +12,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d202310271dcf62b32c1243a23d90c3d1d7b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("HC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HC")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
