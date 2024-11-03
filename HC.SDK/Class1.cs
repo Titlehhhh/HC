9d3c97be03a7fe1411d85547bb7c9ca17f466b22
TitleHHHH <TitleHHHH@outlook.com>
2024-11-03 13:30:53 +0500

    asdasd

--- HC.SDK/Class1.cs
+++ HC.SDK/Class1.cs
@@ -4,7 +4,7 @@ public class MinecraftClient
 {
     public void gaq()
     {
-        Console.WriteLine("asdasd11kk1aaa1111");
+        Console.WriteLine("asdasasdasdasdasdasdasdd11kk1aaa1111");
     }
     public Task GG(int a)
     {
